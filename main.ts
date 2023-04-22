d5232b6c5174ead518471e9ec3b35684806615b0
am1006 <am1006@users.noreply.github.com>
2023-04-22 13:46:52 +1000

    Update my own configs

--- main.ts
+++ main.ts
@@ -1,79 +1,97 @@
 import { serve } from "https://deno.land/std@0.181.0/http/server.ts";
 
 // The name of your Azure OpenAI Resource.
-const resourceName:string = "your-resource-name";
+const resourceName = Deno.env.get("RESOURCE_NAME") || "null";
+
 // The version of OpenAI API.
-const apiVersion:string = "2023-03-15-preview";
+const apiVersion = Deno.env.get("API_VERSION") || "2021-03-15-preview";
+
 // The mapping of model name.
-const mapper:any = {
-  'gpt-3.5-turbo': 'gpt35',
-  'gpt-4': 'gpt4' 
+type MapperType = {
+  [key: string]: string;
+};
+const mapper: MapperType = {
+  "gpt-3.5-turbo": "gpt_35",
+  "gpt-4": "gpt_4",
+  "gpt-4-32k": "gpt_4_32k",
   // Other mapping rules can be added here.
 };
 
-async function handleRequest(request:Request):Promise<Response> {
-  if (request.method === 'OPTIONS') {
-    return handleOPTIONS(request)
+async function handleRequest(request: Request): Promise<Response> {
+  if (request.method === "OPTIONS") {
+    return handleOPTIONS(request);
   }
 
   const url = new URL(request.url);
-  let path:string;
-  if (url.pathname === '/v1/chat/completions') {
-    path="chat/completions"
-  } else if (url.pathname === '/v1/completions') {
-    path="completions"
-  } else if (url.pathname === '/v1/models') {
-    return handleModels(request)
+
+  let path: string;
+
+  if (url.pathname === "/v1/chat/completions") {
+    // Chat
+    path = "chat/completions";
+  } else if (url.pathname === "/v1/completions") {
+    // Other completions
+    path = "completions";
+  } else if (url.pathname === "/v1/models") {
+    // Others
+    return await handleModels(request);
   } else {
-    return new Response('404 Not Found', { status: 404 })
+    return new Response("404 Not Found", { status: 404 });
   }
 
-
   // Get the value of the model field and perform mapping.
-  let deployName:string = '';
-  let body:any;
-  if (request.method === 'POST') {
+  let deployName = "";
+
+  let body;
+
+  if (request.method === "POST") {
     body = await request.json();
-    const modelName:string|undefined = body?.model;
+    const modelName: string | undefined = body?.model;
+
     if (modelName) {
       deployName = mapper[modelName] || modelName;
     }
   }
- 
-  const fetchAPI:string = `https://${resourceName}.openai.azure.com/openai/deployments/${deployName}/${path}?api-version=${apiVersion}`;
 
-  const authKey:string|null = request.headers.get('Authorization');
-  if (!authKey) {
-    return new Response("Not allowed", {status: 403});
+  const fetchAPI = `https://${resourceName}.openai.azure.com/openai/deployments/${deployName}/${path}?api-version=${apiVersion}`;
+
+  const authKey: string | null = request.headers.get("Authorization");
+
+  // Add a very basic API key check.
+  if (!authKey || authKey !== Deno.env.get("API_KEY")) {
+    return new Response("Not allowed: Key Error", { status: 403 });
   }
 
-  const payload:RequestInit = {
+  const payload: RequestInit = {
     method: request.method,
     headers: {
       "Content-Type": "application/json",
-      "api-key": authKey.replace('Bearer ', ''),
+      "api-key": Deno.env.get("AZURE_API_KEY") || "",
     },
     body: JSON.stringify(body),
   };
 
   const { readable, writable } = new TransformStream();
-  const response:Response = await fetch(fetchAPI, payload);
+
+  const response: Response = await fetch(fetchAPI, payload);
+
   if (response.body) {
     stream(response.body, writable);
     return new Response(readable, response);
   } else {
-    throw new Error('Response body is null');
+    throw new Error("Response body is null");
+  }
 }
 
-}
-
-
-function sleep(ms:number):Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, ms));
+function sleep(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 // support printer mode and add newline
-async function stream(readable:ReadableStream<Uint8Array>, writable:WritableStream<Uint8Array>):Promise<void> {
+async function stream(
+  readable: ReadableStream<Uint8Array>,
+  writable: WritableStream<Uint8Array>
+): Promise<void> {
   const reader = readable.getReader();
   const writer = writable.getWriter();
 
@@ -105,49 +123,54 @@ async function stream(readable:ReadableStream<Uint8Array>, writable:WritableStre
   if (buffer) {
     await writer.write(encoder.encode(buffer));
   }
-  await writer.write(encodedNewline)
+  await writer.write(encodedNewline);
   await writer.close();
 }
 
-async function handleModels(request:Request):Promise<Response> {
-  const data:any = {
-    "object": "list",
-    "data": [ {
-      "id": "gpt-3.5-turbo",
-      "object": "model",
-      "created": 1677610602,
-      "owned_by": "openai",
-      "permission": [{
-        "id": "modelperm-M56FXnG1AsIr3SXq8BYPvXJA",
-        "object": "model_permission",
-        "created": 1679602088,
-        "allow_create_engine": false,
-        "allow_sampling": true,
-        "allow_logprobs": true,
-        "allow_search_indices": false,
-        "allow_view": true,
-        "allow_fine_tuning": false,
-        "organization": "*",
-        "group": null,
-        "is_blocking": false
-      }],
-      "root": "gpt-3.5-turbo",
-      "parent": null
-    }]
+function handleModels(_: Request): Response {
+  const data = {
+    object: "list",
+    data: [
+      {
+        id: "gpt-3.5-turbo",
+        object: "model",
+        created: 1677610602,
+        owned_by: "openai",
+        permission: [
+          {
+            id: "modelperm-M56FXnG1AsIr3SXq8BYPvXJA",
+            object: "model_permission",
+            created: 1679602088,
+            allow_create_engine: false,
+            allow_sampling: true,
+            allow_logprobs: true,
+            allow_search_indices: false,
+            allow_view: true,
+            allow_fine_tuning: false,
+            organization: "*",
+            group: null,
+            is_blocking: false,
+          },
+        ],
+        root: "gpt-3.5-turbo",
+        parent: null,
+      },
+    ],
   };
-  const json:string = JSON.stringify(data, null, 2);
+  const json: string = JSON.stringify(data, null, 2);
+
   return new Response(json, {
-    headers: { 'Content-Type': 'application/json' },
+    headers: { "Content-Type": "application/json" },
   });
 }
 
-function handleOPTIONS(request:Request):Response {
-    return new Response(null, {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': '*',
-        'Access-Control-Allow-Headers': '*'
-      }
-    })
+function handleOPTIONS(_: Request): Response {
+  return new Response(null, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Headers": "*",
+    },
+  });
 }
 serve(handleRequest);
